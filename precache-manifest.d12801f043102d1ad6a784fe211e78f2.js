f33d0fc14e19f33a550c074d74522e3a1e1c086d
Hong Tran <tranhong08@gmail.com>
2019-10-19 12:46:03 +0700

    init

---
+++ precache-manifest.d12801f043102d1ad6a784fe211e78f2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74785164bb6149ed37cec7a724edfc48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3af62ef7476a347875af",
+    "url": "/static/css/main.33d6ab8d.chunk.css"
+  },
+  {
+    "revision": "6df3bc3a48ca29e64dc6",
+    "url": "/static/js/2.f9bf20ad.chunk.js"
+  },
+  {
+    "revision": "3af62ef7476a347875af",
+    "url": "/static/js/main.ee2f0387.chunk.js"
+  },
+  {
+    "revision": "c0d72b9585277df44107",
+    "url": "/static/js/runtime-main.bdf3a142.js"
+  }
+]);
